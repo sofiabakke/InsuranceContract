67b1bb3dc85b32a15df9af01b13bc77d45fc3d20
Sofia Bakke <sofiabakke@gmail.com>
2020-09-05 14:31:52 +0200

    Starter files

---
+++ src/main/java/com/example/InsuranceContract/InsuranceContractApplication.java
@@ -0,0 +1,13 @@
+package com.example.InsuranceContract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceContractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceContractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InsuranceContract/InsuranceContractApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.InsuranceContract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class InsuranceContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(1, 2);
+	}
+
+}
