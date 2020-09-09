2bd8490a241d552c1b250fc863f022b0c384c758
Sofia <sofiabakke@gmail.com>
2020-09-09 20:54:08 +0200

    test lagt til

--- src/test/java/com/example/InsuranceContract/InsuranceContractApplicationTests.java
+++ src/test/java/com/example/InsuranceContract/InsuranceContractApplicationTests.java
@@ -7,10 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class InsuranceContractApplicationTests {
+		InsuranceContractResource icr = new InsuranceContractResource();
 
 	@Test
-	void contextLoads() {
-		assertEquals(1, 2);
+	void sendingLegalContractNameShouldReturnSuccess() {
+
+		assertEquals(icr.createContract("Inbo", "Sofia"), "Contract 98765 Sent");
+	}
+
+	@Test
+	void sendingIllegalContractNameShouldReturnFail(){
+		assertEquals(icr.createContract("Ingen", "Sofia"), "Contract 98765 not sent. Invalid name");
 	}
 
 }
